2c47aaa13a29707ed8a2cccd7e9f563f269c4ba6
GeorgePowhattan <martinek.ondrej@gmail.com>
2020-01-12 22:30:52 +0100

    add

--- CartPole/CartPole.py
+++ CartPole/CartPole.py
@@ -9,6 +9,7 @@ from tflearn.layers.estimator import regression
 from statistics import median, mean
 from collections import Counter
 '''
+
 LR = 1e-3
 env = gym.make("CartPole-v0")
 env.reset()
@@ -17,7 +18,7 @@ env.reset()
 goal_steps = 500
 score_requirement = 50
 initial_games = 10000
-
+'''
 def some_random_games_first():
     # Each of these is its own game.
     for episode in range(5):
@@ -44,3 +45,34 @@ def some_random_games_first():
                 break
                 
 some_random_games_first()
+'''
+
+scores = []
+choices = []
+
+for each_game in range(10):
+    score = 0
+    game_memory = []
+    prev_obs = []
+    env.reset()
+    for _ in range(goal_steps):
+        env.render()
+
+        if len(prev_obs)==0:
+            action = random.randrange(0,2)
+        else:
+            action = np.argmax(model.predict(prev_obs.reshape(-1,len(prev_obs),1))[0])
+
+        choices.append(action)
+                
+        new_observation, reward, done, info = env.step(action)
+        prev_obs = new_observation
+        game_memory.append([new_observation, action])
+        score+=reward
+        if done: break
+
+    scores.append(score)
+
+print('Average Score:',sum(scores)/len(scores))
+print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices)))
+print(score_requirement)
