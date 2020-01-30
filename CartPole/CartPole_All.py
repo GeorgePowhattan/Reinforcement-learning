74ad8cfb6317d1038037492633b8c537abf03021
Ondřej Martínek <46747310+GeorgePowhattan@users.noreply.github.com>
2020-01-30 22:06:12 +0100

    update

--- CartPole/CartPole_All.py
+++ CartPole/CartPole_All.py
@@ -2,16 +2,9 @@ import gym
 import random
 import numpy as np
 
-import tensorflow
-import tflearn
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
-from statistics import median, mean
-from collections import Counter
-
 from Model import neural_network_model, train_model
+from Initial_population import initial_population
 
-# LR = 1e-3
 env = gym.make("CartPole-v0")
 env.reset()
 
@@ -23,94 +16,36 @@ initial_games = 10000
 scores = []
 choices = []
 
-def initial_population():
+if __name__ == "__main__":
     
-    training_data = []        # [OBSERVATION, ACTION]
-    scores = []
-    accepted_scores = []      # just the scores that met our threshold:
-    
-    # iterate through however many games we want:
-    for _ in range(initial_games):
+    # filter initial training data through randomly good games
+    training_data = initial_population()
+    # train NN based on the training data
+    model = train_model(training_data)
+
+    for each_game in range(10):
         score = 0
-        # moves specifically from this environment:
         game_memory = []
-        # previous observation that we saw
-        prev_observation = []
-        # for each frame in max 200
-        for _ in range(goal_steps):
-            # choose random action (0 or 1)
-            action = random.randrange(0,2)
-            # do it!
-            observation, reward, done, info = env.step(action)
-            
-            # notice that the observation is returned FROM the action so we'll store the previous observation here, pairing the prev observation to the action we'll take.
-            if len(prev_observation) > 0 :
-                game_memory.append([prev_observation, action])
-            prev_observation = observation
-            score += reward
-            if done:
-                break
-
-        # IF our score is higher than our threshold, we'd like to save every move we made.
-        # NOTE the reinforcement methodology here. 
-        # all we're doing is reinforcing the score, we're not trying to influence the machine in any way as to HOW that score is reached.
-        if score >= score_requirement:
-            accepted_scores.append(score)
-            for data in game_memory:
-                # convert to one-hot (this is the output layer for our neural network)
-                if data[1] == 1:     # if action was 1
-                    output = [0,1]
-                elif data[1] == 0:   # if action was 0
-                    output = [1,0]
-                    
-                # saving our training data
-                training_data.append([data[0], output])
-
-        # reset env to play again
+        prev_obs = []
         env.reset()
-        # save overall scores
+        for _ in range(goal_steps):
+            env.render()
+            
+            if len(prev_obs)==0:   # the first action is random
+                action = random.randrange(0,2)
+            else:
+                action = np.argmax( model.predict(prev_obs.reshape(-1,len(prev_obs),1)) [0] )
+
+            choices.append(action)
+
+            new_observation, reward, done, info = env.step(action)
+            prev_obs = new_observation
+            game_memory.append([new_observation, action])
+            score+=reward
+            if done: break
+
         scores.append(score)
-    
-    # just in case you wanted to reference later
-    training_data_save = np.array(training_data)
-    np.save('saved.npy', training_data_save)
-    
-    # some stats here, to further illustrate the neural network magic!
-    print('Average accepted score:',mean(accepted_scores))
-    print('Median score for accepted scores:',median(accepted_scores))
-    print(Counter(accepted_scores))
-    print(np.array(training_data))
-    
-    return training_data
 
-
-training_data = initial_population()
-
-model = train_model(training_data)
-
-for each_game in range(10):
-    score = 0
-    game_memory = []
-    prev_obs = []
-    env.reset()
-    for _ in range(goal_steps):
-        env.render()
-
-        if len(prev_obs)==0:
-            action = random.randrange(0,2)
-        else:
-            action = np.argmax(model.predict(prev_obs.reshape(-1,len(prev_obs),1))[0])
-
-        choices.append(action)
-                
-        new_observation, reward, done, info = env.step(action)
-        prev_obs = new_observation
-        game_memory.append([new_observation, action])
-        score+=reward
-        if done: break
-
-    scores.append(score)
-
-print('Average Score:',sum(scores)/len(scores))
-print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices)))
-print(score_requirement)
+    print('Average Score:',sum(scores)/len(scores))
+    print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices)))
+    print(score_requirement)
--- CartPole/Model.py
+++ CartPole/Model.py
@@ -1,5 +1,5 @@
 import numpy as np
-import tensorflow
+import tensorflow as tf
 import tflearn
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
